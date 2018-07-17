a953317a1a60e8f10b182398f737a71e98dd4f8b
dla1434 <dla1434@naver.com>
2018-07-17 14:21:37 +0900

    add handler 404 error

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 from .models import Question
 
 from django.template import loader
+
+from django.http import Http404
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -13,7 +15,8 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request,  question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question,pk=question_id)
+    return render(request, 'polls/detail.html', {'question' : question})
 
 def results(request, question_id):
     response = "You're looking at question %s."
@@ -21,8 +24,3 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
-
-def index_join(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
